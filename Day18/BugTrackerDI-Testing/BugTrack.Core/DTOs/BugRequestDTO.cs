d83e029cfd5d24bfd9c9686ba31221ed806b0c8b
adhnanjeff <adhnanjeff26@gmail.com>
2025-08-18 12:07:32 +0530

    feat: adding day 18

---
+++ Day18/BugTrackerDI-Testing/BugTrack.Core/DTOs/BugRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTrack.Core.DTOs
+{
+    public class BugRequestDTO
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int ProjectId { get; set; }
+        // public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string Status { get; set; } = "Open"; 
+    }
+}
