0dc5b01754727e4fa3b04635b4bca9b981adeabb
adhnanjeff <adhnanjeff26@gmail.com>
2025-08-25 15:09:02 +0530

    feat: added registration in hostelManagement

---
+++ Day22/HostelManagementSystem-Reg/Hostel.Core/DTOs/RegistrationRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Hostel.Core.DTOs
+{
+    public class RegistrationRequestDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ Day22/HostelManagementSystem-Reg/Hostel.MVC/Models/StudentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Hostel.MVC.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int? RoomId { get; set; }
+    }
+}
